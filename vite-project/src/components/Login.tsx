81c1fecc484d0466771639dfa396134364f82b1b
fumiyanokesinn <shindori238@gmail.com>
2024-02-06 23:35:35 +0900

    add UILibrary

---
+++ vite-project/src/components/Login.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@yamada-ui/react";
+
+export const Login = () => {
+  return (
+    <div>
+      <Box p="md" rounded="md" bg="#d3d5da" color="black">
+        ログイン ページ
+      </Box>
+    </div>
+  );
+};
