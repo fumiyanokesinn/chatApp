d4d4bda6ec5f220d9ef29d505cac89cc12e828fc
fumiyanokesinn <shindori238@gmail.com>
2024-02-07 22:36:30 +0900

    fix layout

--- vite-project/src/components/Login.tsx
+++ vite-project/src/components/Login.tsx
@@ -1,18 +1,40 @@
-import { Box, Container, Heading, Input } from "@yamada-ui/react";
+import { Box, Button, Container, Flex, Heading, Input } from "@yamada-ui/react";
 import { Header } from "./Header";
+import { useState } from "react";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
+
   return (
     <div>
       <div>
         <Header title="ログインページ" />
       </div>
       <div className="absolute inset-0 flex items-center justify-center">
-        <Box p="md" rounded="md" bg="#d3d5da" color="black">
+        <Box p="md" rounded="md" bg="#d3d5da" color="black" className="w-1/4">
           <Container>
-            <Heading size="md">ログイン</Heading>
-            <Input placeholder="メールアドレス" bg="white" />
-            <Input placeholder="パスワード" bg="white" />
+            <Heading size="lg">Third Chat</Heading>
+            <Input
+              placeholder="メールアドレス"
+              bg="white"
+              onChange={onChangeEmail}
+            />
+            <Input
+              placeholder="パスワード"
+              bg="white"
+              onChange={onChangePassword}
+            />
+            <Flex justify="center">
+              <Button colorScheme="success">ログイン</Button>
+            </Flex>
           </Container>
         </Box>
       </div>
