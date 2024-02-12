4450af8905f555a34ff095350ad8dcaaccc886ee
fumiyanokesinn <shindori238@gmail.com>
2024-02-12 17:15:27 +0900

    Input change

--- vite-project/src/components/Login.tsx
+++ vite-project/src/components/Login.tsx
@@ -1,9 +1,21 @@
-import { Box, Button, Container, Flex, Heading, Input } from "@yamada-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useBoolean,
+} from "@yamada-ui/react";
 import { useState } from "react";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [show, { toggle }] = useBoolean();
 
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.currentTarget.value);
@@ -23,16 +35,29 @@ export const Login = () => {
         >
           <Container>
             <Heading size="lg">Sign in to your account</Heading>
-            <Input
-              placeholder="メールアドレス"
-              bg="#434851"
-              onChange={onChangeEmail}
-            />
-            <Input
-              placeholder="パスワード"
-              bg="#434851"
-              onChange={onChangePassword}
-            />
+            <FormControl label="Email address">
+              <Input
+                placeholder="example@company.com"
+                bg="#434851"
+                onChange={onChangeEmail}
+              />
+            </FormControl>
+            <FormControl label="Password">
+              <InputGroup>
+                <Input
+                  pr="4.5rem"
+                  placeholder="••••••••"
+                  bg="#434851"
+                  onChange={onChangePassword}
+                  type={show ? "text" : "password"}
+                />
+                <InputRightElement w="4.5rem" isClick>
+                  <Button h="1.75rem" size="sm" onClick={toggle}>
+                    {show ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
             <Flex justify="center">
               <Button colorScheme="success">ログイン</Button>
             </Flex>
