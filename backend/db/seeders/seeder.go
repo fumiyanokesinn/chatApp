10296316a408c1a31d55d3a1daeea1bc1e8eed66
fumiyanokesinn <shindori238@gmail.com>
2024-02-17 23:04:08 +0900

    add seeder

---
+++ backend/db/seeders/seeder.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("Seederを実行します")
+	defer fmt.Println("Seederを終了します")
+
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/chat_db")
+	if err != nil {
+		fmt.Println("DBに接続できませんでした")
+		panic(err)
+	}
+	defer db.Close()
+}
