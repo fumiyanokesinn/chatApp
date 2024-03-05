76713f715a6c77a7d8b2329d68d787ca70b76df1
fumiyanokesinn <shindori238@gmail.com>
2024-03-05 18:33:28 +0900

    コメント追加

--- backend/app/api/router.go
+++ backend/app/api/router.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func SetRouter() *gin.Engine {
 	r := gin.Default()
+	// API動作確認用
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Health check OK",
