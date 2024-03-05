e70306fcb0139ea276ca7a7b88502701bcdd6844
fumiyanokesinn <shindori238@gmail.com>
2024-03-05 18:16:48 +0900

    API定義

--- backend/app/main.go
+++ backend/app/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	app "github.com/fumiyanokesinn/chatApp/api"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := app.SetRouter()
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
