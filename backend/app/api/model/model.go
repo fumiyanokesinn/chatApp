c524e5e312967b50ce37facb13565d342bf30d43
fumiyanokesinn <shindori238@gmail.com>
2024-03-08 18:42:52 +0900

    model修正

--- backend/app/api/model/model.go
+++ backend/app/api/model/model.go
@@ -3,17 +3,25 @@ package model
 import (
 	"database/sql"
 	"fmt"
+	"sync"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var (
+	once sync.Once // インスタンスの初期化を一度だけ行うための変数
+	db   *sql.DB   // データベース接続インスタンス
+)
+
+// ConnectDBはデータベース接続のシングルトンインスタンスを返します。
 func ConnectDB() *sql.DB {
-
-	db, err := sql.Open("mysql", "root:root@tcp(db:3306)/chat_db")
-	if err != nil {
-		fmt.Println("DBに接続できませんでした")
-		panic(err)
-	}
-
+	once.Do(func() {
+		var err error
+		db, err = sql.Open("mysql", "root:root@tcp(db:3306)/chat_db")
+		if err != nil {
+			fmt.Println("DBに接続できませんでした")
+			panic(err)
+		}
+	})
 	return db
 }
