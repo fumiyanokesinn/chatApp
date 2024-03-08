875f8bc6a7f53dd8f5480173de2a4a9649f43f14
fumiyanokesinn <shindori238@gmail.com>
2024-03-08 19:51:56 +0900

    失敗パターン追加

--- backend/app/api/model/user/user_repository_test.go
+++ backend/app/api/model/user/user_repository_test.go
@@ -21,10 +21,25 @@ func TestFindByEmail(t *testing.T) {
 
 	user, err := userRepo.FindByEmail("alice@example.com")
 	if err != nil {
-		t.Fatalf("Error getting user: %v", err) // テストをここで終了させる
+		t.Fatalf("Error getting user: %v", err)
 	}
 
 	if user.Name != "Alice" {
 		t.Errorf("検索が上手く行ってません。期待される名前: Alice, 検索結果: %s", user.Name)
+	}
+}
+
+func TestFindByEmailFalse(t *testing.T) {
+	db := model.ConnectDB()
+	userRepo := NewUserRepository(db)
+
+	user, err := userRepo.FindByEmail("false@example.com")
+
+	if user != nil {
+		t.Errorf("検索が成功しています。取得した名前: %s", user.Name)
+	}
+
+	if err.Error() != "ユーザーが見つかりません: false@example.com" {
+		t.Errorf("期待されるエラーメッセージ: 'パスワードが違います, 実際のエラーメッセージ: '%v'", err.Error())
 	}
 }
