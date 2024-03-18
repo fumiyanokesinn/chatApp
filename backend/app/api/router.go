92ed7389e304804ce90a4252fedef6cbbc0c5cfd
fumiyanokesinn <shindori238@gmail.com>
2024-03-18 23:15:50 +0900

    cors分離

--- backend/app/api/router.go
+++ backend/app/api/router.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"net/http"
-
 	myhttp "github.com/fumiyanokesinn/chatApp/api/http"
 	"github.com/fumiyanokesinn/chatApp/api/model"
 	"github.com/fumiyanokesinn/chatApp/api/model/user"
@@ -26,20 +24,7 @@ func SetRouter() *gin.Engine {
 	r.GET("/ping", myhttp.Ping)
 	// 下にエンドポイントを追加
 	r.POST("/login", loginHandler.Login)
-	return r
-}
+	r.POST("/auth/test", loginHandler.Login)
 
-// CORSミドルウェアを定義
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-		c.Next()
-	}
+	return r
 }
